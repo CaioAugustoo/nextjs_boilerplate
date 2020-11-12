a876c0936285ae5fcd8d48bd2fdb7a9fd8bae6e8
caioaugustoo <caioamfr@gmail.com>
2020-11-12 20:38:12 -0300

    Testando para ver se os testes passam

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = "NextJS Boilerplate" }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -10,12 +14,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -56,10 +58,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
