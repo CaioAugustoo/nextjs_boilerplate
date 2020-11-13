cb186cccdb9330b2452652ab71406d673512c706
caioaugustoo <caioamfr@gmail.com>
2020-11-12 22:24:18 -0300

    Estilizações dos componentes

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.main``;
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #ffff;
+
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  padding: 3rem;
+
+`;
+
+export const Logo = styled.img`
+  width: 25rem;
+
+  margin-bottom: 2rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`;
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`;
+
+export const Illustration = styled.img`
+  margin-top: 3rem;
+
+  width: min(30rem, 100%)
+`;
