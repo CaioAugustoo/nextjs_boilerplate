87cffb7ad0d7f5919fa3c82c46cc19be4c0e47a8
caioaugustoo <caioamfr@gmail.com>
2020-12-13 20:52:37 -0300

    Import do Story e Meta do storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,13 +1,14 @@
 import Main from ".";
+import { Story, Meta } from "@storybook/react/types-6-0";
 import { withKnobs, text } from "@storybook/addon-knobs";
 
 export default {
   title: "Main",
   component: Main,
   decorators: [withKnobs],
-};
+} as Meta;
 
-export const Basic = () => (
+export const Basic: Story = () => (
   <Main
     title={text("Title", "React Avançado")}
     description={text(
