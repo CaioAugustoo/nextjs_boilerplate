a6c0b314c639946d91413c43065dd5d1e80d685f
caioaugustoo <caioamfr@gmail.com>
2020-12-13 20:39:22 -0300

    Removida dependência de TypeScript no addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/components/**/stories.tsx"],
   addons: [
-    "@storybook/preset-typescript",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-knobs",
