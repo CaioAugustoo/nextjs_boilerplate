06ba710a19e15611909a11ac06cf868293442243
caioaugustoo <caioamfr@gmail.com>
2020-12-14 12:55:02 -0300

    Uso do storybook essentials e controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/components/**/stories.tsx"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,9 @@
 import Main from ".";
 import { Story, Meta } from "@storybook/react/types-6-0";
-import { withKnobs, text } from "@storybook/addon-knobs";
 
 export default {
   title: "Main",
   component: Main,
-  decorators: [withKnobs],
 } as Meta;
 
-export const Basic: Story = () => (
-  <Main
-    title={text("Title", "React Avançado")}
-    description={text(
-      "Description",
-      "TypeScript, ReactJS, NextJS, Styled Components"
-    )}
-  />
-);
+export const Basic: Story = (args) => <Main {...args} />;
